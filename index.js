aa4cddd53254e8cc3da0cf1e44c5d3b115ee0934
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-25 15:59:45 -0600

    used more DOM practice to change color and text based on prompt input variable.

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const displayText = () => {
     const messageSection = document.getElementById("pageHeader");
     messageSection.innerHTML = pageHeader;
     //if- the input is equal to 'Dane', print 'successMessage'
-    if (input === `Dane`)
-    messageSection.innerHTML = successMessage;
+    if (input === `Dane`) {
+        messageSection.innerHTML = successMessage;
+        messageSection.className = "message successMessage"
+        messageSection.textContent =successMessage;
+        //success = blue text
+    }
+
     //if- the input does is not equal to 'Dane, print 'errorMessage
     else {
         messageSection.innerHTML = errorMessage;
+        messageSection.className = "message errorMessage";
+        messageSection.textContent = errorMessage;
+        //error = red text
     }
     
 }
