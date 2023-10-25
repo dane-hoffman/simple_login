eef8d0e1ded88af88c326df4f3f4a627725612e3
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-25 15:18:00 -0600

    Connected successMessage/errorMessage to print to screen in HTML

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 //Variables
-const loginMessage = String `Please enter your credentials:`;
-const input = prompt (loginMessage);
 const errorMessage = String `Not Authorized`;
 const successMessage = String `Access Granted`;
-const pageHeader = `Hello World`;
+const input = prompt (`Please enter your credentials:`);
 
 //Define Functions
 const displayText = () => {
     const messageSection = document.getElementById("pageHeader");
     messageSection.innerHTML = pageHeader;
+    //if- the input is equal to 'Dane', print 'successMessage'
+    if (input === `Dane`)
+    messageSection.innerHTML = successMessage;
+    //if- the input does is not equal to 'Dane, print 'errorMessage
+    else {
+        messageSection.innerHTML = errorMessage;
+    }
+    
 }
 
 window.addEventListener(`load`, displayText);
@@ -21,6 +27,3 @@ const userIdCheck = (input) => {
         console.log(errorMessage);
     }
 }
-
-//Call Funcitons
-userIdCheck(input);
